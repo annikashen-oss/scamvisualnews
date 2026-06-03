bccd6925d5e9736a281130cebd03f4bd3933e756
annikashen-oss <annika.shen@gmail.com>
2026-06-04 01:16:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // 這是 GitHub Pages 部署必備的設定
-  // 您的專案名稱是 scamvisualnews，所以這裡必須這樣設定，網頁才不會破圖
-  base: '/scamvisualnews/', 
+base: '/scamvisualnews/',
 })
