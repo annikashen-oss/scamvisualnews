64094e79d97e4fcc045e6d9eb0071db402eb1df2
annikashen-oss <annika.shen@gmail.com>
2026-08-27 23:37:32 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // 全域樣式（含 Tailwind + 基礎樣式）
 
-// 如果未來需要使用 GSAP 的 ScrollTrigger，可在這裡註冊
+// 如果未來需要使用GSAP 的 ScrollTrigger，可在這裡註冊
 // import gsap from 'gsap';
 // import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // gsap.registerPlugin(ScrollTrigger);
