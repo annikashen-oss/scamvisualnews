dd726f6ceeff718af52dbf17ef2955bb87710459
annikashen-oss <annika.shen@gmail.com>
2026-08-28 00:04:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-base: '/scamvisualnews/',
+  plugins: [react()],
+  base: '/scamvisualnews/',
 })
