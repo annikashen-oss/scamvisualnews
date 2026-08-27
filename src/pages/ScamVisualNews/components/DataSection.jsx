01d775a637320ecd5e5b722c9876b72add859c2d
annikashen-oss <annika.shen@gmail.com>
2026-08-28 00:59:09 +0800

    Update DataSection.jsx

--- src/pages/ScamVisualNews/components/DataSection.jsx
+++ src/pages/ScamVisualNews/components/DataSection.jsx
@@ -102,7 +102,7 @@ export default function DataSection() {
 
           {/* 搶票測驗 */}
           <div className="reveal-item w-full bg-black/30 rounded-2xl p-8 border border-white/10 text-center my-12">
-            <h5 className="text-xl font-bold text-[#FCE788] mb-4">🎮 搶票大作戰 </h5>
+            <h5 className="text-xl font-bold text-[#FCE788] mb-4">🎫搶票大作戰 </h5>
             <TinderGame />
           </div>
 
