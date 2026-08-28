372fed3c6ae1f35fbe34917ca5c3e2bc4f1a3196
annikashen-oss <annika.shen@gmail.com>
2026-08-28 19:51:25 +0800

    Update DataSection.jsx

--- src/pages/ScamVisualNews/components/DataSection.jsx
+++ src/pages/ScamVisualNews/components/DataSection.jsx
@@ -238,6 +238,7 @@ export default function DataSection() {
               </div>
            </div>
           </div>
+        </div> 
       </section>
 
       {/* ============================================================
